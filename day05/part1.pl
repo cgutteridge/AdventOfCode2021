9a92159aee6e93ef4f08937912a2869979faf75e
cgutteridge <christophergutteridge@gmail.com>
2021-12-05 13:37:50 +0000

    added point class

--- day05/part1.pl
+++ day05/part1.pl
@@ -15,14 +15,15 @@ my @rows = readfile( $file );
 my @lines = ();
 foreach my $row ( @rows ) { 
 	my( $start_text, $end_text ) = split( / -> /, $row );
-	my $start = [split(/,/, $start_text)];
-	my $end = [split(/,/, $end_text)];
+	my @start = split(/,/, $start_text);
+	my @end = split(/,/, $end_text);
 	# convert to ints
-	$start->[0]+=0;
-	$start->[1]+=0;
-	$end->[0]+=0;
-	$end->[1]+=0;
-	push @lines, Line->new( $start, $end );
+	my $start_point = Point->new( @start );
+	my $end_point = Point->new( @end );
+
+	my $line = Line->new( $start_point, $end_point );
+	print "* ".$line->text."\n";
+	push @lines, $line;
 }
 
 my $grid = Grid->new;
@@ -34,7 +35,7 @@ foreach my $line ( @lines ) {
 	}
 }
 
-$grid->draw if $file eq "test";
+$grid->draw if $file =~ m/^test/;
 
 my $n = $grid->overlapping_points;
 
@@ -66,9 +67,9 @@ sub overlapping_points {
 sub inc {
 	my( $self, $point ) = @_;
 
-	$self->[$point->[1]]=[] if( !defined $self->[$point->[1]] );
-	$self->[$point->[1]]->[$point->[0]]=0 if( !defined $self->[$point->[1]]->[$point->[0]] );
-	$self->[$point->[1]]->[$point->[0]]++;
+	$self->[$point->y]=[] if( !defined $self->[$point->y] );
+	$self->[$point->y]->[$point->x]=0 if( !defined $self->[$point->y]->[$point->x] );
+	$self->[$point->y]->[$point->x]++;
 }
 
 sub draw {
@@ -90,6 +91,17 @@ sub draw {
 	}
 }
 
+package Point;
+
+sub new {
+	my( $class, $x, $y ) = @_;
+
+	return bless { x=>$x, y=>$y }, $class;
+}
+
+sub y { return $_[0]->{y}; }
+sub x { return $_[0]->{x}; }
+
 package Line;
 
 sub new {
@@ -99,16 +111,27 @@ sub new {
 	return bless $self, $class;
 }
 
+sub text {
+	my( $self ) = @_;
+
+	return sprintf( "(%d,%d)->(%d,%d)", 
+		$self->{start}->x,
+		$self->{start}->y,
+		$self->{end}->x,
+		$self->{end}->y );
+}
+
+
 sub vertical {
 	my( $self ) = @_;
 
-	return $self->{start}->[0] == $self->{end}->[0];
+	return $self->{start}->x == $self->{end}->x;
 }
 
 sub horizontal {
 	my( $self ) = @_;
 
-	return $self->{start}->[1] == $self->{end}->[1];
+	return $self->{start}->y == $self->{end}->y;
 }
 
 sub points {
@@ -117,15 +140,15 @@ sub points {
 	my @points = ();
 
 	if( $self->vertical ) {
-		my @ys = sort ( $self->{start}->[1] , $self->{end}->[1] );
+		my @ys = sort ( $self->{start}->y , $self->{end}->y );
 		for( my $y = $ys[0]; $y<=$ys[1]; $y++ ) {
-			push @points, [ $self->{start}->[0], $y ];
+			push @points, Point->new( $self->{start}->x, $y );
 		}
 	} 
 	elsif( $self->horizontal ) {
-		my @xs = sort ( $self->{start}->[0] , $self->{end}->[0] );
+		my @xs = sort ( $self->{start}->x , $self->{end}->x );
 		for( my $x = $xs[0]; $x<=$xs[1]; $x++ ) {
-			push @points, [ $x,$self->{start}->[1] ];
+			push @points, Point->new( $x,$self->{start}->y );
 		}
 	} 
 	else {
