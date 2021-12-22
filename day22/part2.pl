6528aa68227045411191bd4807bfb213cd6d6229
cgutteridge <christophergutteridge@gmail.com>
2021-12-22 16:17:41 +0000

    22 pt 2

--- day22/part2.pl
+++ day22/part2.pl
@@ -21,7 +21,6 @@ my @rows = readfile( $file );
 # End of boilerplate
 ############################################################
 
-my $set = {};
 my $rules = [];
 #on x=-30..22,y=-28..20,z=-17..37
 foreach my $row ( @rows ) {
@@ -30,33 +29,116 @@ foreach my $row ( @rows ) {
 	( $rule->{mode}, $rule->{x}->{min}, $rule->{x}->{max}, $rule->{y}->{min}, $rule->{y}->{max}, $rule->{z}->{min}, $rule->{z}->{max} ) = ( $1,$2,$3,$4,$5,$6,$7 );
 	push @$rules,$rule;
 }
-foreach my $rule ( @$rules ) {
-print Dumper( $rule);
-	for(my $z=$rule->{z}->{min};$z<=$rule->{z}->{max};++$z) {
-		next if( abs($z)>50);
-		print "z=$z\n";
-		for(my $y=$rule->{y}->{min};$y<=$rule->{y}->{max};++$y) {
-			next if( abs($y)>50);
-			for(my $x=$rule->{x}->{min};$x<=$rule->{x}->{max};++$x) {
-				next if( abs($x)>50);
-				if( $rule->{mode} eq "off" ) {
-					delete $set->{"$x,$y,$z"};
-				} else {
-					$set->{"$x,$y,$z"}=1;
+
+############################################################
+
+my @zones = ();
+# zones are a list of ON or OFF areas. We do the rules backwards so new zones may not intersect with existing zones
+RULE: foreach my $rule ( reverse @$rules ) {
+	my @zones_to_add = ( $rule );
+
+	ADD: while( @zones_to_add ) {
+		# for next zone, if it doesn't overlap any existing zones, add it,
+		# otherwise split it on the first zone it intersects and add those zones to the todo list
+		# ?? unless it's off and the other zone is off too?
+		my $zone_to_add = shift @zones_to_add;
+		#print "consider ".zonetext($zone_to_add)."\n";
+		ZONE: foreach my $zone ( @zones ) {
+
+			# do they intersect?
+			next if( $zone_to_add->{x}->{max} < $zone->{x}->{min} );
+			next if( $zone_to_add->{x}->{min} > $zone->{x}->{max} );
+			next if( $zone_to_add->{y}->{max} < $zone->{y}->{min} );
+			next if( $zone_to_add->{y}->{min} > $zone->{y}->{max} );
+			next if( $zone_to_add->{z}->{max} < $zone->{z}->{min} );
+			next if( $zone_to_add->{z}->{min} > $zone->{z}->{max} );
+
+			# if fully contained then don't add
+			if( $zone_to_add->{x}->{min} >= $zone->{x}->{min} 
+			 && $zone_to_add->{x}->{max} <= $zone->{x}->{max} 
+			 && $zone_to_add->{y}->{min} >= $zone->{y}->{min} 
+			 && $zone_to_add->{y}->{max} <= $zone->{y}->{max} 
+			 && $zone_to_add->{z}->{min} >= $zone->{z}->{min} 
+			 && $zone_to_add->{z}->{max} <= $zone->{z}->{max} ) {
+				#print "CONTAINED: don't add\n";
+				next ADD;
+			}
+#     |-----
+#   |--|
+#     |--|
+#      |--|
+#  |--|
+# |--|
+
+#     |-----
+#   AABB
+#     BBBB
+#      BBBB
+#  AAAB
+# AAAA
+#     |-----
+
+			# there's 6 possible ways to split - on the min and max planes of the existing zone
+			foreach my $axis ( qw/ x y z / ) {
+				my $add1 = { 
+					mode=>$zone_to_add->{mode}, 
+					x=>$zone_to_add->{x},
+					y=>$zone_to_add->{y},
+					z=>$zone_to_add->{z} };
+				my $add2 = { 
+					mode=>$zone_to_add->{mode}, 
+					x=>$zone_to_add->{x},
+					y=>$zone_to_add->{y},
+					z=>$zone_to_add->{z} };
+				# $axis min
+				if( $zone_to_add->{$axis}->{min} <  $zone->{$axis}->{min} 
+			 	 && $zone_to_add->{$axis}->{max} >= $zone->{$axis}->{min} ) {
+					# split on the zone's x-min plane
+					$add1->{$axis} = { min=>$zone_to_add->{$axis}->{min}, max=>$zone->{$axis}->{min}-1 },
+					$add2->{$axis} = { min=>$zone->{$axis}->{min},        max=>$zone_to_add->{$axis}->{max} },
+					unshift @zones_to_add, $add1;
+					unshift @zones_to_add, $add2;
+					next ADD;
+				}
+				# $axis max
+				if( $zone_to_add->{$axis}->{min} <= $zone->{$axis}->{max} 
+			 	 && $zone_to_add->{$axis}->{max} >  $zone->{$axis}->{max} ) {
+					$add1->{$axis} = { min=>$zone_to_add->{$axis}->{min}, max=>$zone->{$axis}->{max} },
+					$add2->{$axis} = { min=>$zone->{$axis}->{max}+1,      max=>$zone_to_add->{$axis}->{max} },
+					unshift @zones_to_add, $add1;
+					unshift @zones_to_add, $add2;
+					next ADD;
 				}
 			}
 		}
+
+		#print "ADD\n";	
+		# no intersect
+		push @zones, $zone_to_add;
 	}
-}	
-$n = scalar keys %$set;
+}
+		
+foreach my $zone ( @zones ) {
+	next if $zone->{mode} eq "off";
+	my $size_x = $zone->{x}->{max}-$zone->{x}->{min}+1;
+	my $size_y = $zone->{y}->{max}-$zone->{y}->{min}+1;
+	my $size_z = $zone->{z}->{max}-$zone->{z}->{min}+1;
+	$n += $size_x * $size_y * $size_z;
+}
 
 
 ############################################################
 # Output
 ############################################################
 my  $elapsed = tv_interval ( $t0, [gettimeofday]);
-print sprintf( "PART1 => %d\n", $n );
+print sprintf( "PART2 => %d\n", $n );
 print sprintf( "Elapsed time => %fs\n", $elapsed );
 exit;
 ############################################################
 
+
+sub zonetext {
+	my( $zone ) = @_;
+
+	return sprintf( "x=%d..%d y=%d..%d z=%d..%d   (%s)",$zone->{x}->{min},$zone->{x}->{max},$zone->{y}->{min},$zone->{y}->{max},$zone->{z}->{min},$zone->{z}->{max},$zone->{mode} );
+}
